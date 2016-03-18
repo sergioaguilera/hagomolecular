9c084b9f92be57c6c9d3745091a853b5bdb0e23b
Sergio Aguilera <sergio.aaguilera@gmail.com>
2016-03-18 01:00:46 -0600

    Init Repositorio
    Iniciamos repositorio de HAGO molecular

---
+++ contacto.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Hago Molecular - Laboratorio de diagnostico molecular</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Teko:600' rel='stylesheet' type='text/css'>
+
+    <!-- Custom CSS -->
+    <link href="css/small-business.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar  navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"><i class="fa fa-bars fa-2x"></i> 
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    <img src="img/logonav.png" class="img-responsive" width="95" alt="">
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.html">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="nosotros.html">Nosotros</a>
+                    </li>
+                    <li>
+                        <a href="diagnostico.html">Diagnostico Molecular</a>
+                    </li>
+                    <li>
+                        <a href="biotecnologia.html">Biotecnología</a>
+
+                    </li>
+                    <li>
+                        <a href="boletines.html">Boletines</a>
+                    </li>
+                    <li>
+                        <a href="contacto.php">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+        <h1>Contacto</h1>
+        
+        <!-- Heading Row -->
+        <div class="row">
+            <div class="col-md-7">
+
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Formulario de Contacto</h4>
+      </div>
+      <div class="modal-body">
+
+<form name="contact" method="post" action="send_form_email.php">
+  <div class="form-group">
+    <label for="exampleInputPassword1">Nombre Completo</label>
+    <input type="text" class="form-control" id="name" name="name" placeholder="Nombre completo">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Correo Electronico</label>
+    <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+  </div>
+    <div class="form-group">
+    <label for="exampleInputPassword1">Telefono </label>
+    <input type="text" class="form-control" id="tel" name="tel" placeholder="Celular o de domicilio">
+  </div>
+    <div class="form-group">
+    <label for="exampleInputPassword1">Ciudad y estado </label>
+    <input type="text" class="form-control" id="city" name="city" placeholder="Ejemplo: Torreon,Coahuila">
+  </div>
+    
+    <label for="text1">¿En que podemos ayudarte? </label>
+    <textarea class="form-control" rows="3" name="comments" id="comments"></textarea>
+
+ <br>
+  <button type="submit" class="btn btn-default right">Enviar</button>
+ <br>
+ <br>
+ </form>      </div>
+    </div>
+  </div>
+</div>
+                
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3600.0008535544175!2d-103.44233558541566!3d25.538348483738673!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x868fdbdcd64a570f%3A0xa54ece02b179609a!2sAv+Ju%C3%A1rez+751%2C+Segundo+de+Cobi%C3%A1n+Centro%2C+27000+Torre%C3%B3n%2C+Coah.!5e0!3m2!1ses-419!2smx!4v1455733790601" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                 
+                <!-- Button trigger modal -->
+                
+<button type="button" class="btn btn-primary btn-lg center" data-toggle="modal" data-target="#myModal">
+¿Tienes alguna duda o comentario?</button>
+            </div>
+            <!-- /.col-md-8 -->
+            <div class="col-md-5" >
+                <h4 id="mat">Matriz</h4>
+                <p  class="hide" id="matriz">Tel. 01 (871) 2042628
+                    <br>
+                    Cel. 8711745363
+                    <br>
+                    Av. Juárez 751 oriente colonia Centro 
+                    <br>
+                    Torreón Coahuila. C.P. 27000 
+                    <br>
+                    lunes a viernes de 8:00 A.M. A 6:00 P.M. 
+                    <br>
+                    sábados de 9:00 A.M. A 2:00 P.M.
+                    <br>
+                    <br>
+                    Correos:
+                    <br>
+                    direccion@hagomolecular.com
+                    <br>
+                    administracion@hagomolecular.com
+                    <br>
+                    laboratorio@hagomolecular.com
+                    <br>
+                    ventas@hagomolecular.com
+                    <br>
+                    recepcion@hagomolecular.com
+                    <br>
+                </p>
+                <hr>
+                <h4 id="agu" class="tabclick">Aguascalientes</h4>
+                <p id="aguascalientes" class="hide">Dr. Habacuc Hernández
+                    <br>
+                    4931030384
+                    <br>
+                    habacuc@hagomolecular.com
+                </p>
+                <hr>
+                <h4 id="baj" class="tabclick">Baja California</h4>
+                <p id="bajacalifornia" class="hide">Q.F.B. Cecilia Haro
+                    <br>
+                    6643699149
+                    Av. Josefa Ortiz de Domínguez 779
+                    <br>
+                    Col. Centro Tijuana B.C. 
+                    <br>
+                    C.P. 22000
+                    <br>
+                    ventas@hagomolecular.com
+                </p>
+                <hr>
+                <h4 id="cam" class="tabclick">Campeche</h4>
+                <p id="campeche" class="hide">Dra. Fabiola Calderón Zúñiga
+                    <br>
+                    8712314613
+                    <br>
+                    ventas@hagomolecular.com
+                </p>
+                <hr>
+                <h4 id="coa" class="tabclick">Coahuila</h4>
+                <p id="coahuila" class="hide">Tel. 01 (871) 2042628
+                    Cel. 8711745363
+                    <br>
+                    Av. Juárez 751 oriente colonia Centro Torreón Coahuila. 
+                    <br>
+                    C.P. 27000 
+                    <br>
+                    lunes a viernes de 8:00 A.M. A 6:00 P.M. sábados de 9:00 
+                    A.M. A 2:00 P.M.
+                </p>
+                <hr>
+                <h4 id="dfc" class="tabclick">D.F / Centro</h4>
+                <p id="dfcentro" class="hide">Dr. Iván Alejandro Córdoba Solís
+                    <br>
+                    5523442787
+                    <br>
+                    ventas@hagomolecular.com
+                </p>
+                <hr>
+                <h4 id="dur" class="tabclick">Durango</h4>
+                <p id="durango" class="hide">Q.F.B. Edith Sandoval Carreón
+                    <br>
+                    6181531366
+                    <br>
+                    ventas@hagomolecular.com
+                </p>
+                <hr>
+                <h4 id="gua" class="tabclick">Guanajuato</h4>
+                <p  id="guanajuato" class="hide">M.C. Estephany Tondopo Jiménez
+                    <br>
+                    4621104952
+                    <br>
+                    ventas@hagomolecular.com
+                </p>
+                <hr>
+                <h4  id="nue" class="tabclick">Nuevo León</h4>
+                <p  id="nuevoleon" class="hide">Dr. Iván Alberto Marino Martínez
+                    <br>
+                    8113874103
+                    <br>
+                    ventas@hagomolecular.com
+                </p>
+                <hr>
+                <h4  id="san" class="tabclick">San Luis Potosí</h4>
+                <p  id="sanluis" class="hide">Lic. Vicenta Zapata
+                    <br>
+                    4444343194
+                    <br>
+                    ventas@hagomolecular.com
+                </p>
+                <hr>
+                <h4  id="tam" class="tabclick">Tamaulipas</h4>
+                <p  id="tamaulipas" class="hide">Dr. Iván Alberto Marino Martínez
+                    <br>
+                    8113874103
+                    <br>
+                    ventas@hagomolecular.com
+                </p>
+                <hr>
+                <h4 id="zac" class="tabclick">Zacatecas</h4>
+                <p  id="zacatecas" class="hide">Dr. Habacuc Hernández
+                    <br>
+                    4931030384
+                    <br>
+                    habacuc@hagomolecular.com
+                </p>
+
+            </div>
+            <!-- /.col-md-4 -->
+        </div>
+        <!-- /.row -->
+        <hr>
+   <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-11">
+                    <p>Copyright &copy; HAGO molecular 2016 / Conoce nuestro <a href="" data-toggle="modal" data-target="#privacidad">Aviso de privacidad</a></p>
+                </div>
+            </div>
+        </footer>
+    
+    <!-- Modal  Aviso-->
+<div class="modal fade" id="privacidad" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Aviso de Privacidad</h4>
+      </div>
+      <div class="modal-body">
+<h2>Aviso de Privacidad HAGO molecular</h2>
+<p><strong>Estimado Cliente,</strong></p>
+
+<p>En virtud de que <strong>HAGO molecular</strong>, es responsable de recabar y tratar datos personales que ha obtenido o que llegue a obtener, hemos implementado mecanismos para garantizar la seguridad de sus datos personales (los <strong>“Datos Personales”</strong>); por tal razón, hacemos el siguiente:</p>
+ 
+<p><strong>Aviso de Privacidad</strong></p>
+
+<p>Con fundamento en los artículos 15, 16 y demás aplicables de la Ley Federal de Protección de Datos Personales en Posesión de Particulares (la <strong>“Ley”</strong>) y su reglamento hacemos de su conocimiento que <strong>HAGO molecular </strong>, con domicilio en Av. Juarez 751 Colonia Centro Torreon, Coahuila, Mexico, teléfono 01 (871) 2042628, y página web www.hagomolecular.com, es responsable de recabar, del uso y la protección de <strong>Datos Personales</strong>, en cumplimiento de los principios de licitud, calidad, consentimiento, información, finalidad, lealtad, proporcionalidad y responsabilidad.</p>
+
+<p>Nuestros clientes proporcionan <strong>Datos Personales</strong> a <strong>HAGO Molecular</strong>, de manera directa, indirecta, personal o a través de sus subsidiarias, filiales, afiliadas, filiales, controladoras y/o aliadas comerciales.</p>
+
+<p>Los <strong>Datos Personales</strong> se recopilan a través de: oficinas corporativas; correo electrónico y/o teléfono, y podrán incluir: (a) nombre(s) y apellidos; (b) dirección de correo electrónico; (c) números telefónicos (de trabajo, casa y móvil);  (d) comprobante de domicilio; y (e) identificación oficial.</p>
+
+<p>Los fines primarios aplicables a los <strong>Datos Personales</strong> (los <strong>“Fines Primarios”</strong>) serán: (a) identificarlo como cliente o representante legal; (b) elaborar documentos, contratos, convenios, facturas, recibos y documentación relacionada con la relación;  (c) almacenamiento; y, (d) dar cumplimiento a términos y condiciones que hayamos establecido con usted.</p>
+
+<p>Los fines secundarios aplicables a los <strong>Datos Personales</strong> (los <strong>“Fines Secundarios”</strong>) serán: (a) estadística; (b) mercadeo y (c) prospección.</p>
+
+<p>La confidencialidad de los <strong>Datos Personales</strong> está garantizada y los mismos están protegidos por medidas de seguridad administrativas, técnicas y/o físicas, para evitar daño, pérdida, alteración, destrucción, uso, acceso o divulgación indebida, por ejemplo los <strong>Datos Personales</strong> se encuentran en una base de datos interna, cuya administración es a través de claves de acceso que cambian en forma periódica y cuyo acceso está restringido a personas autorizadas; convenios de confidencialidad con su personal, entre otras.</p>
+
+<p>Usted tiene derecho al acceso, rectificación y cancelación de sus <strong>Datos Personales</strong>, a oponerse al tratamiento de los mismos o a revocar su consentimiento (en su conjunto <strong>“Derechos ARCO”</strong>). Para ello, es necesario que usted o su representante legal presente una  Solicitud por escrito del ejercicio del <strong>Derecho ARCO</strong> dirigida al área de atención a clientes de <strong>HAGO molecular</strong>, responsable de la protección de <strong>Datos Personales</strong>, ubicado en el domicilio antes indicado, o bien, se comunique vía correo electrónico al área de atención a clientes de <strong>HAGO molecular</strong>      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    
+    <script>
+        
+    $("#mat").click(function(){
+       $("#matriz").removeClass("hide");
+    });
+    
+    $("#coa").click(function(){
+       $("#coahuila").removeClass("hide");
+    });
+        
+    $("#agu").click(function(){
+       $("#aguascalientes").removeClass("hide");
+    });
+    
+    $("#zac").click(function(){
+       $("#zacatecas").removeClass("hide");
+    });
+    
+    $("#baj").click(function(){
+       $("#bajacalifornia").removeClass("hide");
+    });
+        
+    $("#cam").click(function(){
+       $("#campeche").removeClass("hide");
+    });
+        
+    $("#dfc").click(function(){
+       $("#dfcentro").removeClass("hide");
+    });
+        
+    $("#dur").click(function(){
+       $("#durango").removeClass("hide");
+    });
+        
+    $("#gua").click(function(){
+       $("#guanajuato").removeClass("hide");
+    });
+        
+    $("#nue").click(function(){
+       $("#nuevoleon").removeClass("hide");
+    });
+    
+    $("#san").click(function(){
+       $("#sanluis").removeClass("hide");
+    });
+    
+    $("#tam").click(function(){
+       $("#tamaulipas").removeClass("hide");
+    });
+        
+    $("#zac").click(function(){
+       $("#zacatecas").removeClass("hide");
+    });
+    </script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
